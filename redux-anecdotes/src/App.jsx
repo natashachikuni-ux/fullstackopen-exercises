3970766028d0808fc6e3f2c12a5299be7559d789
natashachikuni-ux <natasha.chikuni@july28.co.zw.>
2026-04-23 11:55:26 +0200

    style: finish part 7 with react-bootstrap styling

--- redux-anecdotes/src/App.jsx
+++ redux-anecdotes/src/App.jsx
@@ -1,3 +1,4 @@
+import { Table, Alert, Navbar, Nav } from 'react-bootstrap'
 import { useState } from 'react'
 import {
   Routes, Route, Link, useMatch, useNavigate
@@ -6,45 +7,38 @@ import {
 // 1. THE CUSTOM HOOK
 const useField = (type) => {
   const [value, setValue] = useState('')
+  const onChange = (event) => { setValue(event.target.value) }
+  const reset = () => { setValue('') }
 
-  const onChange = (event) => {
-    setValue(event.target.value)
-  }
-
-  const reset = () => {
-    setValue('')
-  }
-
-  return {
-    type,
-    value,
-    onChange,
-    reset
-  }
+  return { type, value, onChange, reset }
 }
 
+// 2. SUB-COMPONENTS
 const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
-    <ul>
-      {anecdotes.map(anecdote => 
-        <li key={anecdote.id}>
-          <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
-        </li>
-      )}
-    </ul>
+    <Table striped bordered hover>
+      <tbody>
+        {anecdotes.map(anecdote => (
+          <tr key={anecdote.id}>
+            <td>
+              <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+            </td>
+            <td>{anecdote.author}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
   </div>
 )
 
-const Anecdote = ({ anecdote }) => {
-  return (
-    <div>
-      <h2>{anecdote.content} by {anecdote.author}</h2>
-      <div>has {anecdote.votes} votes</div>
-      <p>for more info see <a href={anecdote.info}>{anecdote.info}</a></p>
-    </div>
-  )
-}
+const Anecdote = ({ anecdote }) => (
+  <div>
+    <h2>{anecdote.content} by {anecdote.author}</h2>
+    <div>has {anecdote.votes} votes</div>
+    <p>for more info see <a href={anecdote.info}>{anecdote.info}</a></p>
+  </div>
+)
 
 const About = () => (
   <div>
@@ -54,7 +48,6 @@ const About = () => (
   </div>
 )
 
-// 2. UPDATED CREATENEW USING CUSTOM HOOKS
 const CreateNew = ({ addNew }) => {
   const content = useField('text')
   const author = useField('text')
@@ -72,7 +65,6 @@ const CreateNew = ({ addNew }) => {
     navigate('/')
   }
 
-  // New reset handler
   const handleReset = (e) => {
     e.preventDefault()
     content.reset()
@@ -80,51 +72,42 @@ const CreateNew = ({ addNew }) => {
     info.reset()
   }
 
-  // We "destructure" the reset out of the object so it's not passed to the input
-  // Only 'type', 'value', and 'onChange' remain in the 'spread' variable
-  const { reset: contentReset, ...contentInput } = content
-  const { reset: authorReset, ...authorInput } = author
-  const { reset: infoReset, ...infoInput } = info
+  const { reset: _, ...contentInput } = content
+  const { reset: __, ...authorInput } = author
+  const { reset: ___, ...infoInput } = info
 
   return (
     <div>
       <h2>create a new anecdote</h2>
       <form onSubmit={handleSubmit}>
-        <div>
-          content: <input {...contentInput} /> 
-        </div>
-        <div>
-          author: <input {...authorInput} />
-        </div>
-        <div>
-          url for info: <input {...infoInput} />
-        </div>
-        <button type="submit">create</button>
-        <button type="button" onClick={handleReset}>reset</button>
+        <div className="mb-2">content: <input className="form-control" {...contentInput} /></div>
+        <div className="mb-2">author: <input className="form-control" {...authorInput} /></div>
+        <div className="mb-2">url: <input className="form-control" {...infoInput} /></div>
+        <button className="btn btn-primary me-2" type="submit">create</button>
+        <button className="btn btn-secondary" type="button" onClick={handleReset}>reset</button>
       </form>
     </div>
   )
 }
+
+// 3. MAIN APP COMPONENT
 const App = () => {
   const [anecdotes, setAnecdotes] = useState([
     {
       content: 'If it hurts, do it more often',
       author: 'Jez Humble',
       info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
-      votes: 0,
-      id: 1
+      votes: 0, id: 1
     },
     {
       content: 'Premature optimization is the root of all evil',
       author: 'Donald Knuth',
       info: 'http://wiki.c2.com/?PrematureOptimization',
-      votes: 0,
-      id: 2
+      votes: 0, id: 2
     }
   ])
 
   const [notification, setNotification] = useState('')
-
   const match = useMatch('/anecdotes/:id')
   const anecdote = match 
     ? anecdotes.find(a => a.id === Number(match.params.id))
@@ -137,22 +120,21 @@ const App = () => {
     setTimeout(() => setNotification(''), 5000)
   }
 
-  const padding = { padding: 5 }
-
   return (
-    <div>
-      <h1>Software anecdotes</h1>
-      <div>
-        <Link style={padding} to="/">anecdotes</Link>
-        <Link style={padding} to="/create">create new</Link>
-        <Link style={padding} to="/about">about</Link>
-      </div>
+    <div className="container">
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="mb-3">
+        <Navbar.Brand className="ms-3">Anecdote App</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">anecdotes</Nav.Link>
+            <Nav.Link as={Link} to="/create">create new</Nav.Link>
+            <Nav.Link as={Link} to="/about">about</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
 
-      {notification && (
-        <div style={{ border: '2px solid red', padding: 10, margin: '10px 0' }}>
-          {notification}
-        </div>
-      )}
+      {notification && <Alert variant="success">{notification}</Alert>}
 
       <Routes>
         <Route path="/anecdotes/:id" element={<Anecdote anecdote={anecdote} />} />
@@ -161,9 +143,9 @@ const App = () => {
         <Route path="/about" element={<About />} />
       </Routes>
 
-      <div style={{ marginTop: 20 }}>
+      <footer className="mt-5 p-4 bg-light text-center">
         <i>Anecdote app, for <a href='https://fullstackopen.com/'>Full Stack Open</a>.</i>
-      </div>
+      </footer>
     </div>
   )
 }
